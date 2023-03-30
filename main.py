7ab0be3599a141ee2805ef034ee0dd899f827519
sainandan <sainandan888@gmail.com>
2023-03-30 10:07:03 +0530

    third commit and i created method to extract data from postman to backend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from fastapi.params import Body
 app=FastAPI()
 
 @app.get('/')
@@ -7,5 +7,6 @@ def root():
     return {'message':"helloword"}
 
 @app.put('/createpost')
-def create_first_post():
-    return {"message":"succesfully craeted posts"}
+def create_first_post(payload:dict=Body(...)):
+    print(payload)
+    return {"message":f"title :{payload['title']} and content :{payload['content']}"}
