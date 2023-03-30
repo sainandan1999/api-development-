ebe465a1e9990b677776342eeb20c205c1f426af
sainandan <sainandan888@gmail.com>
2023-03-30 12:51:57 +0530

    this code for deleteing the code

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,status,HTTPException,Response
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
@@ -10,6 +10,12 @@ def finding_post(id):
     for i in mylist:
         if i["id"]==id:
             return i
+        
+def finding_the_index(id):
+    for i,p in enumerate(mylist):
+        if p['id']==id:
+            return i
+    return -1
 
 class Post(BaseModel):
     title:str
@@ -20,7 +26,7 @@ class Post(BaseModel):
 app=FastAPI()
 
 #create a post
-@app.post('/posts')
+@app.post('/posts',status_code=status.HTTP_201_CREATED)
 def create_posts(post:Post):
     new_post=post.dict()
     new_post["id"]=randrange(3,10)
@@ -38,5 +44,17 @@ def get_posts():
 @app.get('/post/{id}')
 def get_post(id:int):
     post=finding_post(id)
-    print(post)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with id:{id} was not found")
     return {"message":post}
+
+
+
+#deleting the post
+@app.delete("/posts/{id}",status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id:int):
+    index=finding_the_index(id)
+    mylist.pop(index)
+    if index==None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with id:{id} doesnot exits")
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
