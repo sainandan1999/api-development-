bbab61aaf9441e63001f766aeb25c0f32c71cbf4
sainandan <sainandan888@gmail.com>
2023-03-30 11:12:58 +0530

    this is crud system with read method to get all the posts from the databse but currently not using the database instud iusig list

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 
+mylist=[{"title":"title of the first post","content":"this is content of first posts","id":1},
+{"title":"title of the second post","content":"this is content of second posts","id":2}]
+
 
 class Post(BaseModel):
     title:str
@@ -17,3 +20,8 @@ def create_posts(post:Post):
     print(post)
     print(post.dict())
     return {"data":post}
+
+#read a post form the list beacuse now to using database for retiving the data
+@app.get('/posts')
+def get_posts():
+    return {"message":mylist}
